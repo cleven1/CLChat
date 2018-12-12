969f10593425220bec14cd1f9971ccc911a52440
赵永强 <clevenzhao@gmail.com>
2018-12-12 20:30:01 +0800

    搭建框架

---
+++ app/src/main/java/com/cleven/clchat/clchat/fragment/CLContactFragment.java
@@ -0,0 +1,33 @@
+package com.cleven.clchat.clchat.fragment;
+
+import android.graphics.Color;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.cleven.clchat.clchat.base.CLBaseFragment;
+
+/**
+ * Created by cleven on 2018/12/11.
+ */
+
+public class CLContactFragment extends CLBaseFragment {
+    @Override
+    public View initView() {
+
+        TextView textView = new TextView(mContext);
+        textView.setGravity(Gravity.CENTER);
+        textView.setText("联系人");
+        textView.setTextSize(40);
+        textView.setTextColor(Color.BLACK);
+
+        return textView;
+    }
+
+    @Override
+    public void initData() {
+        super.initData();
+
+        System.out.println("加载数据");
+    }
+}
