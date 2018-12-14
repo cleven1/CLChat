eb765312f003a03180c8a3629f35e6c6da67b944
赵永强 <clevenzhao@gmail.com>
2018-12-14 20:08:49 +0800

    添加聊天界面布局

--- app/src/main/java/com/cleven/clchat/clchat/home/activity/CLSessionActivity.java
+++ app/src/main/java/com/cleven/clchat/clchat/home/activity/CLSessionActivity.java
@@ -2,14 +2,10 @@ package com.cleven.clchat.clchat.home.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.cleven.clchat.clchat.R;
-import com.cleven.clchat.clchat.utils.CLMQTTService.CLMQTTManager;
-
-import java.util.HashMap;
 
 public class CLSessionActivity extends AppCompatActivity {
 
@@ -23,35 +19,35 @@ public class CLSessionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_session);
 
-        editText = (EditText) findViewById(R.id.et_text);
-
-        btn_connect = (Button) findViewById(R.id.btn_connect);
-        btn_connect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // 连接
-                CLMQTTManager.getInstance().connect();
-            }
-        });
-
-        disconnect = (Button) findViewById(R.id.btn_disconnect);
-        disconnect.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                CLMQTTManager.getInstance().disconnect();
-            }
-        });
-
-        btn_send = (Button) findViewById(R.id.btn_send);
-        btn_send.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                HashMap<String,String> dict = new HashMap<>();
-                dict.put("id","12312312");
-                dict.put("msg",editText.getText().toString().trim());
-                CLMQTTManager.getInstance().sendMessage("2",dict);
-            }
-        });
+//        editText = (EditText) findViewById(R.id.et_text);
+//
+//        btn_connect = (Button) findViewById(R.id.btn_connect);
+//        btn_connect.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // 连接
+//                CLMQTTManager.getInstance().connect();
+//            }
+//        });
+//
+//        disconnect = (Button) findViewById(R.id.btn_disconnect);
+//        disconnect.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                CLMQTTManager.getInstance().disconnect();
+//            }
+//        });
+//
+//        btn_send = (Button) findViewById(R.id.btn_send);
+//        btn_send.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                HashMap<String,String> dict = new HashMap<>();
+//                dict.put("id","12312312");
+//                dict.put("msg",editText.getText().toString().trim());
+//                CLMQTTManager.getInstance().sendMessage("2",dict);
+//            }
+//        });
 
     }
 
