81fd8c958523f38e3a511a93178a8be582e9782b
赵永强 <zyq@gailvlun.com>
2018-12-18 14:50:39 +0800

    完善user模型

--- app/src/main/java/com/cleven/clchat/model/CLUserBean.java
+++ app/src/main/java/com/cleven/clchat/model/CLUserBean.java
@@ -15,9 +15,39 @@ public class CLUserBean {
      */
     private String name;
     /**
+     * 性别
+     *  0: 女
+     *  1: 男
+     */
+    private int gender;
+    /**
+     * 生日
+     */
+    private String birthday;
+    /**
+     * 别名
+     */
+    private String aliasName;
+    /**
+     * 签名
+     */
+    private String description;
+    /**
      * 头像
      */
     private String avatarUrl;
+    /**
+     * 城市
+     */
+    private String city;
+    /**
+     * token
+     */
+    private String token;
+    /**
+     * 手机号
+     */
+    private String mobilePhone;
 
     public String getUserId() {
         return userId;
@@ -41,5 +71,61 @@ public class CLUserBean {
 
     public void setAvatarUrl(String avatarUrl) {
         this.avatarUrl = avatarUrl;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAliasName() {
+        return aliasName;
+    }
+
+    public void setAliasName(String aliasName) {
+        this.aliasName = aliasName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
     }
 }
