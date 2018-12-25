970fc44ed9d86eead3e1901b85c86c5edb052bd2
赵永强 <clevenzhao@gmail.com>
2018-12-25 21:51:20 +0800

    添加联系人界面

--- app/src/main/java/com/cleven/clchat/fragment/CLContactFragment.java
+++ app/src/main/java/com/cleven/clchat/fragment/CLContactFragment.java
@@ -1,33 +1,120 @@
 package com.cleven.clchat.fragment;
 
-import android.graphics.Color;
-import android.view.Gravity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
-import android.widget.TextView;
 
+import com.cleven.clchat.R;
 import com.cleven.clchat.base.CLBaseFragment;
+import com.cleven.clchat.fragment.contack.adpater.ContactsAdapter;
+import com.cleven.clchat.fragment.contack.model.ContactModel;
+import com.cleven.clchat.fragment.contack.views.PinnedHeaderDecoration;
+import com.nanchen.wavesidebar.SearchEditText;
+import com.nanchen.wavesidebar.Trans2PinYinUtil;
+import com.nanchen.wavesidebar.WaveSideBarView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by cleven on 2018/12/11.
  */
 
 public class CLContactFragment extends CLBaseFragment {
+
+    private List<ContactModel> mContactModels;
+    private List<ContactModel> mShowModels;
+    private RecyclerView mRecyclerView;
+    private WaveSideBarView mWaveSideBarView;
+    private SearchEditText mSearchEditText;
+    private ContactsAdapter mAdapter;
+
     @Override
     public View initView() {
+        View view = View.inflate(mContext, R.layout.contact_layout,null);
 
-        TextView textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setText("联系人");
-        textView.setTextSize(40);
-        textView.setTextColor(Color.BLACK);
+        bindView(view);
 
-        return textView;
+        return view;
+    }
+
+    private void bindView(View view) {
+
+        // RecyclerView设置相关
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.main_recycler);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        final PinnedHeaderDecoration decoration = new PinnedHeaderDecoration();
+        decoration.registerTypePinnedHeader(1, new PinnedHeaderDecoration.PinnedHeaderCreator() {
+            @Override
+            public boolean create(RecyclerView parent, int adapterPosition) {
+                return true;
+            }
+        });
+        mRecyclerView.addItemDecoration(decoration);
+
+        // 侧边设置相关
+        mWaveSideBarView = (WaveSideBarView) view.findViewById(R.id.main_side_bar);
+        mWaveSideBarView.setTextColor(R.color.black);
+        mWaveSideBarView.setOnSelectIndexItemListener(new WaveSideBarView.OnSelectIndexItemListener() {
+            @Override
+            public void onSelectIndexItem(String letter) {
+                for (int i=0; i<mContactModels.size(); i++) {
+                    if (mContactModels.get(i).getIndex().equals(letter)) {
+                        ((LinearLayoutManager) mRecyclerView.getLayoutManager()).scrollToPositionWithOffset(i, 0);
+                        return;
+                    }
+                }
+            }
+        });
+
+
+        // 搜索按钮相关
+        mSearchEditText = (SearchEditText) view.findViewById(R.id.main_search);
+        mSearchEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                mShowModels.clear();
+                for (ContactModel model : mContactModels) {
+                    String str = Trans2PinYinUtil.trans2PinYin(model.getName());
+                    if (str.contains(s.toString()) || model.getName().contains(s.toString())) {
+                        mShowModels.add(model);
+                    }
+                }
+                mAdapter.notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
     public void initData() {
         super.initData();
+        mContactModels = new ArrayList<>();
+        mShowModels = new ArrayList<>();
+        mContactModels.addAll(ContactModel.getContacts());
+        mShowModels.addAll(mContactModels);
+        mAdapter = new ContactsAdapter(mShowModels);
+        mRecyclerView.setAdapter(mAdapter);
+    }
 
-        System.out.println("加载数据");
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mContactModels != null){
+            mContactModels.clear();
+            mContactModels = null;
+        }
     }
 }
